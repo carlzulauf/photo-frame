09640d57c08f001171431ddf0b7f7d302a1b7712
Carl Zulauf <carl@linkleaf.com>
2013-10-12 00:19:22 -0400

    Add skeleton webos app and debug server

---
+++ webos/server.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+FRAMEWORK_PATH = "/opt/PalmSDK/0.1/share/framework/"
+ENYO_PATH = File.join(FRAMEWORK_PATH, "enyo", "1.0", "framework")
+set :public_folder, File.dirname(__FILE__)
+
+get '/' do
+  send_file 'index.html'
+end
+
+get '/framework_config.json' do
+  send_file File.join(ENYO_PATH, "framework_config.json")
+end
+
+get %r{^/framework/(.+)} do |framework_file|
+  puts "here!!!"
+  send_file File.join(FRAMEWORK_PATH, framework_file)
+end
---
+++ webos/source/PhotoFrame.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+  document.getElementById('debug-box').innerHTML = "Busted!";
+}
